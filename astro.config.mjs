6c3112e3a14c6d7e74585443baf4757ea337d751
bigpigmoon <matvey.novikov02@gmail.com>
2025-05-24 22:27:44 +0700

    add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bigpigmoon.github.io",
+  base: "ezy_mutants",
   integrations: [
     starlight({
       title: "Ezy Mutants",
